9358792033901ed9c7a0174b238e52d0809099ed
RootPark <rmsdnjs518@gmail.com>
2025-09-03 17:52:05 +0900

    PS) NOT SOLVED

---
+++ Daily/D0819/Swea1873.java
@@ -0,0 +1,55 @@
+package Daily.D0819;
+
+import java.io.*;
+import java.util.*;
+
+/*
+문자	의미
+.	평지(전차가 들어갈 수 있다.)
+*	벽돌로 만들어진 벽
+#	강철로 만들어진 벽
+-	물(전차는 들어갈 수 없다.)
+^	위쪽을 바라보는 전차(아래는 평지이다.)
+v	아래쪽을 바라보는 전차(아래는 평지이다.)
+<	왼쪽을 바라보는 전차(아래는 평지이다.)
+>	오른쪽을 바라보는 전차(아래는 평지이다.)
+ */
+
+/*
+문자	동작
+U	Up : 전차가 바라보는 방향을 위쪽으로 바꾸고, 한 칸 위의 칸이 평지라면 위 그 칸으로 이동한다.
+D	Down : 전차가 바라보는 방향을 아래쪽으로 바꾸고, 한 칸 아래의 칸이 평지라면 그 칸으로 이동한다.
+L	Left : 전차가 바라보는 방향을 왼쪽으로 바꾸고, 한 칸 왼쪽의 칸이 평지라면 그 칸으로 이동한다.
+R	Right : 전차가 바라보는 방향을 오른쪽으로 바꾸고, 한 칸 오른쪽의 칸이 평지라면 그 칸으로 이동한다.
+S	Shoot : 전차가 현재 바라보고 있는 방향으로 포탄을 발사한다.
+ */
+
+public class Swea1873 {
+    static int HEIGHT;
+    static int WIDTH;
+    static char[][] map;
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= testCase; t++) {
+            st = new StringTokenizer(br.readLine());
+            HEIGHT = Integer.parseInt(st.nextToken());
+            WIDTH = Integer.parseInt(st.nextToken());
+
+            map = new char[HEIGHT][WIDTH];
+
+            for (int heightIndex = 0; heightIndex < HEIGHT; heightIndex++) {
+                String line = br.readLine();
+                for (int widthIndex = 0; widthIndex < WIDTH; widthIndex++) {
+                    map[heightIndex][widthIndex] = line.charAt(widthIndex);
+                }
+            }
+        }
+    }
+}
